da35a399e1aa3b8b1e67a400b50f4932e06bb958
gonzalo alvarez moreno <g.alvarez33@usp.ceu.es>
2023-06-22 22:15:02 +0200

    final correo verificacion

--- flask/ventanas/main.py
+++ flask/ventanas/main.py
@@ -7,10 +7,10 @@ from itsdangerous import URLSafeTimedSerializer
 app = Flask(__name__)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True  # Si se utiliza TLS
+app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'practicaceu@gmail.com'
 app.config['MAIL_PASSWORD'] = 'pzrwkjjftegjcwwy'
-app.secret_key = '1234'  # Reemplaza con tu propia clave secreta
+app.secret_key = '12345'
 
 mail = Mail(app)
 client = MongoClient('mongodb+srv://gonzaloalv:5OrWE1buHSE3AjAP@tfg.acxkjkk.mongodb.net/')
@@ -18,26 +18,23 @@ db = client['TFG']
 collection = db['usuarios']
 
 
-users = []  # Lista para almacenar usuarios registrados
-
 @app.route('/home')
 def index():
     return render_template('home.html')
 
-@app.route('/login', methods=['GET', 'POST'])
 
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['correo']
         password = request.form['contrasena']
 
-        # Buscar el usuario en la base de datos
-        user_count = collection.count_documents({'correo': f'{username}', 'contraseña': f'{password}'},)
+        user_count = collection.count_documents({'correo': username, 'contraseña': password})
         if user_count > 0:
-           # Almacenar estado de inicio de sesión en la sesión
             return redirect(url_for('restricted'))
         else:
-            return redirect(url_for('login'))
+            error = 'El Correo o Contraseña son incorrectos'
+            return render_template('login.html', error=error)
 
     return render_template('login.html')
 
@@ -54,53 +51,40 @@ def registro():
             error = 'El correo electrónico no tiene el formato válido.'
             return render_template('registro.html', error=error)
 
-    # Validar la longitud del NIA
-        
         if len(nia) != 6 or not nia.isdigit():
-            error= 'El NIA debe ser un número de 6 dígitos.'
+            error = 'El NIA debe ser un número de 6 dígitos.'
             return render_template('registro.html', error=error)
 
-        usuario_existente = collection.count_documents({'correo': f'{correo}'})
-        if usuario_existente >0:
-            error= "El correo electrónico ya está registrado."
+        usuario_existente = collection.count_documents({'correo': correo})
+        if usuario_existente > 0:
+            error = "El correo electrónico ya está registrado."
             return render_template('registro.html', error=error)
 
-        # Validar la contraseña
         if not re.search(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{5,}$', contraseña):
             error = 'La contraseña debe tener al menos 5 caracteres, una mayúscula, una minúscula y un número.'
             return render_template('registro.html', error=error)
 
-
-
-        # Crear el documento del usuario
         datos_usuario = {
             'correo': correo,
             'contraseña': contraseña,
-            'nia': nia
+            'nia': nia,
+            
         }
 
-        # Insertar el documento en la colección de MongoDB Atlas
-        collection.insert_one(datos_usuario)
-        enviar_correo_verificacion(correo)
-        # Redirigir al usuario a la página de inicio de sesión después del registro exitoso
+        enviar_correo_verificacion(correo, contraseña, nia)
         return redirect(url_for('login'))
 
     return render_template('registro.html')
 
 
-def enviar_correo_verificacion(correo):
-    # Generar un token único para el usuario
+def enviar_correo_verificacion(correo, contraseña, nia):
     serializer = URLSafeTimedSerializer(app.secret_key)
     token = serializer.dumps(correo, salt='email-confirm')
+    url_verificacion = url_for('confirmar_correo', token=token, contraseña=contraseña, nia=nia, _external=True)
 
-    # Construir el enlace de verificación
-    url_verificacion = url_for('confirmar_correo', token=token, _external=True)
-
-    # Crear el mensaje de correo
     mensaje = Message('Verificación de correo electrónico', sender='ceupractica@gmail.com', recipients=[correo])
     mensaje.body = f'Haz clic en el siguiente enlace para verificar tu correo electrónico: {url_verificacion}'
 
-    # Enviar el correo
     mail.send(mensaje)
 
 
@@ -108,24 +92,29 @@ def enviar_correo_verificacion(correo):
 def confirmar_correo(token):
     serializer = URLSafeTimedSerializer(app.secret_key)
     try:
-        correo = serializer.loads(token, salt='email-confirm', max_age=3600)  # Verificar el token
-        # Realizar la acción correspondiente, por ejemplo, activar la cuenta del usuario en la base de datos
-        # Puedes mostrar un mensaje de confirmación en esta página
+        correo = serializer.loads(token, salt='email-confirm', max_age=3600)
+        contraseña = request.args.get('contraseña')
+        nia = request.args.get('nia')
+        print(correo,contraseña,nia)
+
+        datos_usuario = {
+            'correo': correo,
+            'contraseña': contraseña,
+            'nia': nia
+        }
+
+        collection.insert_one(datos_usuario)
         return render_template('login.html', correo=correo)
     except:
-        # Si el token es inválido o ha expirado, puedes mostrar un mensaje de error
         return render_template('login.html', error='Token inválido o expirado')
-
-
-
 
 
 @app.route('/restricted')
 def restricted():
-    if 'logged_user' in session:
+
         return render_template('restricted.html', logged_user=session['logged_user'])
-    else:
-        return redirect(url_for('login'))
+    
+    
 
 if __name__ == '__main__':
     app.run(port=5004)
