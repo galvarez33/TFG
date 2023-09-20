2f2b9cee2ca19931893f8e368b6d073a9969caf5
gonagon4 <82447045+gonagon4@users.noreply.github.com>
2023-09-20 17:46:09 +0200

    implemnetaciond de api en explorar.py

--- flask/ventanas/api/resources.py
+++ flask/ventanas/api/resources.py
@@ -1,23 +1,12 @@
+# api/resources.py
+
 from flask_restful import Resource
-from routes.explorar import explorar  # Asegúrate de que la importación sea correcta
+from funciones.explorar_funciones import obtener_parametros_dudas
 
 class ExplorarResource(Resource):
     def get(self):
-        # Llamar a la función explorar() para obtener los datos
-        dudas = explorar()
-        
-        
-        # Convertir los datos a un formato JSON adecuado
-        result = []
-        for duda in dudas:
-            duda_json = {
-                'titulo': duda['titulo'],
-                'descripcion': duda['descripcion'],
-                'carrera': duda['carrera'],
-                'curso': duda['curso'],
-                # Agregar otros campos si es necesario
-            }
-            result.append(duda_json)
-        
-        print(result)
-        return {'dudas': result}
+        # Llama a la función obtener_parametros_dudas para obtener los parámetros de las dudas
+        parametros_dudas = obtener_parametros_dudas()
+
+        # Convierte los parámetros de las dudas en una respuesta JSON
+        return {'dudas': parametros_dudas}
--- flask/ventanas/funciones/explorar_funciones.py
+++ flask/ventanas/funciones/explorar_funciones.py
@@ -25,3 +25,28 @@ def obtener_dudas(consulta, carrera, curso, page, per_page):
 
 
     return dudas, total_dudas
+
+
+def obtener_parametros_dudas():
+    client = MongoClient('mongodb+srv://gonzaloalv:5OrWE1buHSE3AjAP@tfg.acxkjkk.mongodb.net/')
+    db = client['TFG']
+    form_collection = db['publicar_duda']
+
+    # Aquí realiza la consulta para obtener las dudas de la base de datos
+    dudas = form_collection.find()
+
+    # Inicializa una lista para almacenar los parámetros de las dudas
+    parametros_dudas = []
+
+    # Recorre las dudas y almacena los parámetros en la lista
+    for duda in dudas:
+        parametros_duda = {
+            'titulo': duda.get('titulo', ''),  # Usa get para manejar claves faltantes
+            'descripcion': duda.get('descripcion', ''),  # Usa get para manejar claves faltantes
+            'carrera': duda.get('carrera', ''),  # Usa get para manejar claves faltantes
+            'curso': duda.get('curso', ''),  # Usa get para manejar claves faltantes
+            # Agrega otros campos si es necesario
+        }
+        parametros_dudas.append(parametros_duda)
+
+    return parametros_dudas
