a718df7da4cb3dde66fac18d79d753c9965ed2fe
gonagon4 <82447045+gonagon4@users.noreply.github.com>
2023-10-10 13:48:50 +0200

    test perfil buscar por id la duda

--- flask/ventanas/test/test_perfil.py
+++ flask/ventanas/test/test_perfil.py
@@ -2,7 +2,8 @@ import pytest
 import sys,os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import pytest
-from funciones.perfil_funciones import obtener_dudas_usuario, form_collection
+from funciones.perfil_funciones import obtener_dudas_usuario, form_collection, borrar_duda_por_id
+from bson import ObjectId
 
 @pytest.fixture
 def form_collection_mock():
@@ -15,10 +16,7 @@ def form_collection_mock():
             self.data.append(document)
 
         def find(self, query):
-            # Simular la búsqueda en la base de datos
             return self.data
-
-    # Reemplazar la colección real con la versión simulada
     form_collection._collection = FormCollectionMock()
     return form_collection
 
@@ -38,3 +36,18 @@ def test_obtener_dudas_usuario(form_collection_mock):
     dudas_cursor = obtener_dudas_usuario(correo_usuario, page, per_page)
     dudas_lista = list(dudas_cursor) 
     assert len(dudas_lista) == 2 
+
+
+def test_borrar_duda_por_id(form_collection_mock):
+    duda_id_prueba = ObjectId() 
+    duda_prueba = {
+        "_id": duda_id_prueba,
+        "correo_usuario": "usuario@example.com",
+        "texto": "Esta es una duda de prueba"
+    }
+    form_collection_mock.insert_one(duda_prueba)
+
+    borrar_duda_por_id(str(duda_id_prueba))  # Convierte el ID en una cadena para asegurar la coincidencia
+
+    duda_eliminada = form_collection_mock.find_one({"_id": duda_id_prueba})
+    assert duda_eliminada is None  # 
