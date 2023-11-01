9949a245e9aa10c10e81be0b28767d2ecd0e85e4
gonagon4 <82447045+gonagon4@users.noreply.github.com>
2023-11-01 10:30:09 +0100

    modelo categorico

--- flask/ventanas/ia/model-copia.py
+++ flask/ventanas/ia/model-copia.py
@@ -1,50 +1,60 @@
 import os
 import numpy as np
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Embedding, LSTM, concatenate, Dense
-from tensorflow.keras.metrics import Accuracy, Recall, Precision, AUC
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Embedding, LSTM, Dense
+from sklearn.preprocessing import LabelEncoder
 
-# Parámetros para preprocesamiento y entrenamiento
-max_text_length = 100  # Ajusta la longitud máxima del texto según tus datos
-vocab_size = 10000  # Ajusta el tamaño del vocabulario según tus datos
-embedding_dim = 128  # Ajusta la dimensión de embedding según tus datos
-batch_size = 32
+# Ruta de la carpeta con los archivos de texto
+carpeta = '/mnt/c/Users/ASUS/Documents/IA/textos'
 
-# Datos de texto preprocesados (ajusta estos datos según tus necesidades)
-train_texts = np.random.randint(0, vocab_size, size=(1000, max_text_length))
-validation_texts = np.random.randint(0, vocab_size, size=(200, max_text_length))
+# Leer los textos de los archivos en la carpeta
+textos = []
+categorias = []
 
-# Construcción del modelo RNN para el texto
-input_text = Input(shape=(max_text_length,))
-embedding_layer = Embedding(input_dim=vocab_size, output_dim=embedding_dim)(input_text)
-lstm_layer = LSTM(128)(embedding_layer)
+for archivo in os.listdir(carpeta):
+    with open(os.path.join(carpeta, archivo), 'r', encoding='utf-8') as file:
+        texto = file.read()
+        textos.append(texto)
+        categorias.append(archivo.split('_')[0])  # El nombre del archivo se usa como categoría
 
-# Construcción del modelo CNN para las imágenes (se mantiene igual)
-# ...
+# Convertir las categorías a números
+label_encoder = LabelEncoder()
+categorias_encoded = label_encoder.fit_transform(categorias)
 
-# Fusiona las salidas de la rama de imágenes y texto
-merged = concatenate([model_img.output, lstm_layer])
+# Tokenización de texto
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(textos)
+total_palabras = len(tokenizer.word_index) + 1
 
-output = Dense(4, activation='softmax')(merged)  # 4 unidades de salida para 4 clases con función de activación softmax
+# Convertir texto a secuencias numéricas
+secuencias = tokenizer.texts_to_sequences(textos)
+
+# Padding para que todas las secuencias tengan la misma longitud
+max_longitud = max([len(seq) for seq in secuencias])
+secuencias_padded = pad_sequences(secuencias, maxlen=max_longitud, padding='post')
+
+# Construcción del modelo RNN
+model = Sequential([
+    Embedding(total_palabras, 64, input_length=max_longitud),
+    LSTM(128),
+    Dense(len(label_encoder.classes_), activation='softmax')  # Salida con función de activación softmax para clasificación no binaria
+])
 
 # Compilación del modelo
-model = Model(inputs=[model_img.input, input_text], outputs=output)
-metrics = [Accuracy(), Recall(), Precision(), AUC()]
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=metrics)
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+# Datos de entrenamiento y etiquetas
+datos_entrenamiento = secuencias_padded
+etiquetas = np.array(categorias_encoded)
 
 # Entrenamiento del modelo
-model.fit(
-    [train_generator, train_texts],
-    steps_per_epoch=train_generator.samples // batch_size,
-    epochs=7,
-    validation_data=([validation_generator, validation_texts],
-                     validation_labels),  # Ajusta según tus datos
-    validation_steps=validation_generator.samples // batch_size
-)
-
-# Calcular las predicciones en el conjunto de validación
-y_pred = model.predict([validation_generator, validation_texts])
-y_true = validation_generator.classes  # Las etiquetas verdaderas del conjunto de validación
+model.fit(datos_entrenamiento, etiquetas, epochs=10, batch_size=1)
 
 # Guardar el modelo
-model.save('modelo-combinado.h5')
+model.save('modelo_texto_no_binario.h5')
+
+# Guardar el encoder para convertir categorías a números durante la predicción
+import joblib
+joblib.dump(label_encoder, 'label_encoder.pkl')
--- flask/ventanas/ia/pdf.py
+++ flask/ventanas/ia/pdf.py
@@ -9,11 +9,11 @@ def extract_text_from_docx(file_path):
     return '\n'.join(full_text)
 
 # Ejemplo de uso
-file_path = '/mnt/c/Users/ASUS/Documents/IA/categorico/programacion_I/programacion_I.docx'  # Reemplaza esto con la ruta real de tu archivo .docx
+file_path = '/mnt/c/Users/ASUS/Documents/IA/categorico/fisica_I/fisica.docx'  # Reemplaza esto con la ruta real de tu archivo .docx
 extracted_text = extract_text_from_docx(file_path)
 
 # Guardar el texto en un archivo .txt
-output_file_path = 'programacion_I.txt'  # Ruta para guardar el archivo .txt
+output_file_path = 'fisica_I.txt'  # Ruta para guardar el archivo .txt
 with open(output_file_path, 'w', encoding='utf-8') as file:
     file.write(extracted_text)
 
