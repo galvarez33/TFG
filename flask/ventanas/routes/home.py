4f66906364b5c6a053a0001072bef857cc334491
gonzalo alvarez moreno <g.alvarez33@usp.ceu.es>
2023-11-19 13:12:41 +0100

    creado vercel3

--- flask/ventanas/routes/home.py
+++ flask/ventanas/routes/home.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, session
 from . import home_bp 
 
-@home_bp.route('/home')
+@home_bp.route('/')
 def home():
     logged_user = session.get('logged_user')
     return render_template('home.html', logged_user=logged_user)
